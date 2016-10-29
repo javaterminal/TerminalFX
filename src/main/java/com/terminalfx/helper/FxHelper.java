48c8df676610ccb9b727fc4aafeaedba299e8076
Rahman Usta <rahman.usta.88@gmail.com>
2016-10-29 14:39:31 +0300

    Update helpers

--- src/main/java/com/terminalfx/helper/FxHelper.java
+++ src/main/java/com/terminalfx/helper/FxHelper.java
@@ -1,6 +1,12 @@
 package com.terminalfx.helper;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.paint.Color;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by usta on 26.09.2016.
@@ -12,5 +18,32 @@ public class FxHelper {
                 (int) (color.getRed() * 255),
                 (int) (color.getGreen() * 255),
                 (int) (color.getBlue() * 255));
+    }
+
+    public static boolean askQuestion(String message) {
+        CompletableFuture<Boolean> completableFuture = new CompletableFuture<Boolean>();
+        completableFuture.runAsync(() -> {
+            ThreadHelper.runActionLater(() -> {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, message, ButtonType.YES, ButtonType.NO);
+                ButtonType buttonType = alert.showAndWait().orElse(ButtonType.NO);
+                completableFuture.complete(buttonType == ButtonType.YES);
+            });
+        });
+
+        return completableFuture.join();
+    }
+
+    public static String askInput(String message) {
+        CompletableFuture<String> completableFuture = new CompletableFuture<String>();
+        completableFuture.runAsync(() -> {
+            ThreadHelper.runActionLater(() -> {
+                TextInputDialog inputDialog = new TextInputDialog();
+                inputDialog.setContentText(message);
+                Optional<String> optional = inputDialog.showAndWait();
+                completableFuture.complete(optional.orElseGet(() -> null));
+            });
+        });
+
+        return completableFuture.join();
     }
 }
--- src/main/java/com/terminalfx/helper/IOHelper.java
+++ src/main/java/com/terminalfx/helper/IOHelper.java
@@ -3,9 +3,7 @@ package com.terminalfx.helper;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.nio.file.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,6 +11,25 @@ import java.util.stream.Stream;
  * Created by usta on 12.09.2016.
  */
 public class IOHelper {
+
+    private static final Set<String> nativeFiles = new HashSet<>();
+
+    static {
+        List<String> freebsd = Arrays.asList("libpty/freebsd/x86/libpty.so", "libpty/freebsd/x86_64/libpty.so");
+        List<String> linux = Arrays.asList("libpty/linux/x86/libpty.so", "libpty/linux/x86_64/libpty.so");
+        List<String> macosx = Arrays.asList("libpty/macosx/x86/libpty.dylib", "libpty/macosx/x86_64/libpty.dylib");
+        List<String> win_x86 = Arrays.asList("libpty/win/x86/winpty.dll", "libpty/win/x86/winpty-agent.exe");
+        List<String> win_x86_64 = Arrays.asList("libpty/win/x86_64/winpty.dll", "libpty/win/x86_64/winpty-agent.exe", "libpty/win/x86_64/cyglaunch.exe");
+        List<String> win_xp = Arrays.asList("libpty/win/xp/winpty.dll", "libpty/win/xp/winpty-agent.exe");
+
+        nativeFiles.addAll(freebsd);
+        nativeFiles.addAll(linux);
+        nativeFiles.addAll(macosx);
+        nativeFiles.addAll(win_x86);
+        nativeFiles.addAll(win_x86_64);
+        nativeFiles.addAll(win_xp);
+    }
+
     public static void close(Closeable... closables) {
         for (Closeable closable : closables) {
             try {
@@ -45,32 +62,16 @@ public class IOHelper {
 
     public static void copyLibPty(Path dataDir) throws IOException {
 
-        List<String> nativeFiles = new ArrayList<>();
-
-        List<String> freebsd = Arrays.asList("libpty/freebsd/x86/libpty.so", "libpty/freebsd/x86_64/libpty.so");
-        List<String> linux = Arrays.asList("libpty/linux/x86/libpty.so", "libpty/linux/x86_64/libpty.so");
-        List<String> macosx = Arrays.asList("libpty/macosx/x86/libpty.dylib", "libpty/macosx/x86_64/libpty.dylib");
-        List<String> win_x86 = Arrays.asList("libpty/win/x86/winpty.dll", "libpty/win/x86/winpty-agent.exe");
-        List<String> win_x86_64 = Arrays.asList("libpty/win/x86_64/winpty.dll", "libpty/win/x86_64/winpty-agent.exe", "libpty/win/x86_64/cyglaunch.exe");
-        List<String> win_xp = Arrays.asList("libpty/win/xp/winpty.dll", "libpty/win/xp/winpty-agent.exe");
-
-        nativeFiles.addAll(freebsd);
-        nativeFiles.addAll(linux);
-        nativeFiles.addAll(macosx);
-        nativeFiles.addAll(win_x86);
-        nativeFiles.addAll(win_x86_64);
-        nativeFiles.addAll(win_xp);
-
         for (String nativeFile : nativeFiles) {
-            InputStream inputStream = IOHelper.class.getResourceAsStream("/" + nativeFile);
             Path nativePath = dataDir.resolve(nativeFile);
 
             if (Files.notExists(nativePath)) {
                 Files.createDirectories(nativePath.getParent());
+                InputStream inputStream = IOHelper.class.getResourceAsStream("/" + nativeFile);
                 Files.copy(inputStream, nativePath);
+                close(inputStream);
             }
 
-            close(inputStream);
         }
 
 
