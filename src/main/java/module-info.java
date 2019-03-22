c6b4f5fdc234a143c3829b891e8e293f4aa0a965
Rahman Usta <rahmanusta@kodedu.com>
2019-03-22 02:50:47 +0100

    Java 11 update

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+open module terminalfx{
+
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires jdk.jsobject;
+    requires com.fasterxml.jackson.databind;
+    requires pty4j;
+    requires jna;
+    requires com.fasterxml.jackson.annotation;
+
+    exports com.kodedu.terminalfx;
+
+}
